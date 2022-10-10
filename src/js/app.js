c9df19ff00731b0ed795d965f4eb808066b77856
Roman Chuchev <romanok20@gmail.com>
2022-10-10 18:31:04 +0300

    feat: add adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
-import * as flsFunctions from './modules'
-
+import * as flsFunctions from "./modules";
+import "./modules/dynamic_adapt.js";
 /* Раскомментировать для использования */
 // import Swiper, { Navigation, Pagination } from 'swiper'
 
-flsFunctions.isWebp()
+flsFunctions.isWebp();
+
+document
+    .querySelector(".header__burger")
+    .addEventListener("click", function () {
+        this.classList.toggle("open");
+        document.querySelector(".header__menu").classList.toggle("open");
+    });
